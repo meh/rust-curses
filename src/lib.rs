7b97e937bac79d0d72c3c5cdcc07e2d87288e32e
meh <meh@schizofreni.co>
2015-12-06 16:11:10 +0100

    crate: export proper modules

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,9 @@ pub use curses as sys;
 
 pub type Result<T> = ::std::result::Result<T, Error>;
 
+#[macro_use]
+mod util;
+
 mod error;
 pub use error::Error;
 
@@ -18,14 +21,29 @@ pub use color::Color;
 pub mod attribute;
 pub use attribute::Attributes;
 
+mod key;
+pub use key::Key;
+
+mod mouse;
+pub use mouse::Mouse;
+
+mod scroll;
+pub use scroll::Scroll;
+
+mod event;
+pub use event::Event;
+
 mod width;
 pub use width::Width;
 
 mod character;
 pub use character::Character;
 
+mod symbol;
+pub use symbol::Symbol;
+
 pub mod screen;
 pub use screen::Screen;
 
-mod window;
+pub mod window;
 pub use window::Window;
