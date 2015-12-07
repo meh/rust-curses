4720221acce5454aaef3161882ca80aaef18172f
meh <meh@schizofreni.co>
2015-12-07 23:36:11 +0100

    screen/suspended: add inline attributes

--- src/screen/suspended.rs
+++ src/screen/suspended.rs
@@ -5,6 +5,7 @@ use super::Screen;
 pub struct Suspended(Screen);
 
 impl Suspended {
+	#[inline]
 	pub fn new(screen: Screen) -> Result<Self, Screen> {
 		unsafe {
 			match Error::check(curses::def_prog_mode()) {
@@ -21,6 +22,7 @@ impl Suspended {
 		}
 	}
 
+	#[inline]
 	pub fn resume(self) -> Result<Screen, Self> {
 		unsafe {
 			match Error::check(curses::reset_prog_mode()) {
