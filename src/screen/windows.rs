c502e19d076e375f6b2c9269777d05da7fb5fcd3
meh <meh@schizofreni.co>
2015-12-08 02:09:36 +0100

    screen/windows: add some helper methods

--- src/screen/windows.rs
+++ src/screen/windows.rs
@@ -32,13 +32,65 @@ impl<'a> Windows<'a> {
 
 	#[inline]
 	pub fn get<S: AsRef<str>>(&mut self, name: S) -> Result<Window> {
-		if self.screen.windows.contains_key(name.as_ref()) {
+		if !self.screen.windows.contains_key(name.as_ref()) {
 			return Err(Error::BadArgument);
 		}
 
 		unsafe {
 			Ok(Window::wrap(*self.screen.windows.get(name.as_ref()).unwrap()))
 		}
+	}
+
+	#[inline]
+	pub fn overlay<S: AsRef<str>, D: AsRef<str>>(&mut self, src: S, dst: D) -> Result<()> {
+		if !self.screen.windows.contains_key(src.as_ref()) || !self.screen.windows.contains_key(dst.as_ref()) {
+			return Err(Error::BadArgument);
+		}
+
+		unsafe {
+			try!(Error::check(curses::overlay(
+				*self.screen.windows.get(src.as_ref()).unwrap(),
+				*self.screen.windows.get(dst.as_ref()).unwrap())));
+		}
+
+		Ok(())
+	}
+
+	#[inline]
+	pub fn overwrite<S: AsRef<str>, D: AsRef<str>>(&mut self, src: S, dst: D) -> Result<()> {
+		if !self.screen.windows.contains_key(src.as_ref()) || !self.screen.windows.contains_key(dst.as_ref()) {
+			return Err(Error::BadArgument);
+		}
+
+		unsafe {
+			try!(Error::check(curses::overwrite(
+				*self.screen.windows.get(src.as_ref()).unwrap(),
+				*self.screen.windows.get(dst.as_ref()).unwrap())));
+		}
+
+		Ok(())
+	}
+
+	pub fn copy<S: AsRef<str>, D: AsRef<str>>(&mut self, src: S, dst: D,
+	                                                     (src_min_col, src_min_row): (u32, u32),
+	                                                     (dst_min_col, dst_min_row): (u32, u32),
+	                                                     (dst_max_col, dst_max_row): (u32, u32),
+	                                                     overlay: bool) -> Result<()> {
+		if !self.screen.windows.contains_key(src.as_ref()) || !self.screen.windows.contains_key(dst.as_ref()) {
+			return Err(Error::BadArgument);
+		}
+
+		unsafe {
+			try!(Error::check(curses::copywin(
+				*self.screen.windows.get(src.as_ref()).unwrap(),
+				*self.screen.windows.get(dst.as_ref()).unwrap(),
+				src_min_row as c_int, src_min_col as c_int,
+				dst_min_row as c_int, dst_min_col as c_int,
+				dst_max_row as c_int, dst_max_col as c_int,
+				if overlay { 1 } else { 0 })));
+		}
+
+		Ok(())
 	}
 
 	#[inline]
